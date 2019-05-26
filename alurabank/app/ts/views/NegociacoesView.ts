d61a1ca4cf2b056d53208065bb9d9983e43a59f3
Palharez <xeduardopalhares@gmail.com>
2019-05-26 19:52:41 -0300

    add curso type 1

---
+++ alurabank/app/ts/views/NegociacoesView.ts
@@ -0,0 +1,34 @@
+class NegociacoesView extends View<Negociacoes> {
+
+    template(model: Negociacoes): string {
+        return `
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                    <th>VOLUME</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                ${model.paraArray().map(negociaco => 
+                    `
+                    <tr>
+                        <td>${negociaco.data.getDate()}/${negociaco.data.getMonth() + 1}/${negociaco.data.getFullYear()}</td>
+                        <td>${negociaco.quantidade}</td>
+                        <td>${negociaco.valor}</td>
+                        <td>${negociaco.volume}</td>
+                    </tr>
+                    `
+                ).join()}
+            </tbody>
+
+            <tfoot>
+            </tfoot>
+        </table>    
+        `;
+    }
+
+}
